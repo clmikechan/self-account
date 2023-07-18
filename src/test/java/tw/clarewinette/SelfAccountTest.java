09b3f669d11a2760de18dd590f8e3cbcaa1eba32
Clare W. Inette <clmikechan@gmail.com>
2023-07-18 16:46:13 +0800

    [feature][test]增加 test 之入口

---
+++ src/test/java/tw/clarewinette/SelfAccountTest.java
@@ -0,0 +1,13 @@
+package tw.clarewinette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SelfAccountTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
